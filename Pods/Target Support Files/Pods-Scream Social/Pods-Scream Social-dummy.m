022f1009f0684fc84dc061b56cf3b5f0006784f7
Rick Crane <rickcrane@Ricks-MacBook-Pro.local>
2017-02-20 19:12:39 +0100

    Completed Setting Up Firebase Intergration

---
+++ Pods/Target Support Files/Pods-Scream Social/Pods-Scream Social-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scream_Social : NSObject
+@end
+@implementation PodsDummy_Pods_Scream_Social
+@end
--- Scream Social/SignInVC.swift
+++ Scream Social/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  Scream Social
 //
 //  Created by Rick Crane on 20/02/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
